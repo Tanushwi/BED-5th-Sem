00c091d12edcb98679b49741e020f53ea723511d
Tanushwi <tanushwisingh27@gmail.com>
2025-08-21 14:21:36 +0530

    Lecture-19

--- lec_19/index.js
+++ lec_19/index.js
@@ -5,21 +5,24 @@ const app=express()
 
 const Users=require("./model/user")
 const Blog=require("./model/blog")
-let blogRoute=require("./routes/blogRoutes")
+
+let blogRoutes=require("./routes/blogRoutes")
 let userRoutes=require("./routes/userRoutes")
+
+console.log(blogRoutes)
+console.log(userRoutes)
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-//create
-app.use("/api/blogs",blogRoute)
-app.use("/api/users",userRoutes)
 
+app.use("/api/blogs",blogRoutes)
+app.use("/api/users",userRoutes)
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/g27dB')
   .then(() => console.log('Connected!'));
 
-app.listen(9999,()=>{
+app.listen(9898,()=>{
     console.log("server started")
 })
--- lec_19/routes/blogRoutes.js
+++ lec_19/routes/blogRoutes.js
@@ -1,68 +1,12 @@
-const express=require('express');
-const router=express.Router(); //small --> app
-const Blog=require("../model/blog")
-let {postAddBlog,getAllBlog,getOneBlog,deleteBlog}=require("../controller/blog")
-require("../controller/blogController")
-router.post("/",async(req,res)=>{
-    let title=req.body.title;
-    let body=req.body.body;
-    let userId=req.body.userId;
-    let blog={
-        title:title,
-        body:body,
-        date:Date.now(),
-        userId:userId
-    }
-    
-    let newBlog=new Blog(blog)
-    await newBlog.save()
-    let user=await Users.findById(userId);
-    user.blogs.push(newBlog._id)
-    await user.save()
+const express=require("express")
+const Blog=require("../model/blog.js")
+const Users=require("../model/user.js")
+let{postAddBlog,getAllBlogs,getOneBlog,deleteOneBlog}=require("../controller/blogController.js")
+const router=express.Router(); //small-->app
 
-    res.json({
-        success:true,
-        message:"blog added successfully",
-        data:newBlog
-    })
-})
-
-router.delete("/:blogId",async(req,res)=>{
-    let blogId=req.params.blogId;
-    let userId=req.body.userId;
-    let blogExists=await Blog.findById(blogId);
-    if(!blogExists){
-        return res.json({
-            success:false,
-            message:"Blog does not exist"
-        })
-    }
-    if(blogExists.userId.toString() !== userId){
-        return res.json({
-            success:false,
-            message:"Permission denied"
-        })
-    }
-    await Blog.findByIdAndDelete(blogId);//hw: remove blogs column from user 
-})
-
-router.get("/",async(req,res)=>{
-    let allBlogs=await Blog.find()
-    res.json({
-        success:true,
-        message:"data added successfully",
-        data:allBlogs
-    })
-})
-
-router.get("/:id",async(req,res)=>{
-    let id=req.params.id;
-    let blog=await Blog.findById(id);
-    res.json({
-        success:true,
-        message:"blog fetched successfully",
-        data:blog
-    })
-})
+router.post("/",postAddBlog)
+router.delete("/:n=blogId",deleteOneBlog)
+router.get("/",getAllBlogs)
+router.get("/:id",getOneBlog)
 
 module.exports=router
