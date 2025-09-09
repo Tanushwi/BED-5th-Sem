301169ab334af4d5ee55d4197d1d6b3a4fa5b048
Tanushwi <tanushwisingh27@gmail.com>
2025-09-09 09:14:00 +0530

    Lecture-23

---
+++ lec_23/routes/authRoutes.js
@@ -0,0 +1,11 @@
+
+const express= require("express");
+const router= express()
+let {postLogin}=require("../controller/authController")
+router.post("/login",postLogin)
+
+
+
+
+
+module.exports=router;
---
+++ lec_23/routes/userRoutes.js
@@ -0,0 +1,9 @@
+
+const express= require("express");
+const router= express();
+const {postAddUser,getAllUsers}= require("../controller/userController")
+router.post("/",postAddUser)
+router.get("/",getAllUsers)
+
+
+module.exports=router;
