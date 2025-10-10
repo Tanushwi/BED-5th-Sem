f2744261d7696c2eb68599bc47fe3c6fe61a47e1
Tanushwi <tanushwisingh27@gmail.com>
2025-10-10 11:23:11 +0530

    Lecture-30

---
+++ lec_30/controller/userController.js
@@ -0,0 +1,11 @@
+
+const User = require("../service/userService")
+
+module.exports.postUser= async (req,res)=>{
+    let {name,email}= req.body;
+   let message= await User.addUser(email,name)
+   res.json({
+    success:true,
+    message:message
+   })
+}
